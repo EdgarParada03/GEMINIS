2002c71b34b7eae4df345516a11f6b046f82061a
Edgar Yamill Parada Duarte <104601278+EdgarParada03@users.noreply.github.com>
2024-05-09 16:44:54 -0500

    Administrador carpet

---
+++ Administrador/Alumno.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Administrador;
+
+/**
+ *
+ * @author edgar
+ */
+public class Alumno extends Persona {
+    private String matricula;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "matricula=" + matricula + '}';
+    }
+
+    
+}
